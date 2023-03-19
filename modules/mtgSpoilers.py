19f8768360de86ecabec998d94a1f070b1d12630
bashgame <amen.kester@gmail.com>
2023-03-19 00:20:20 +0000

    Implemented Async Tests
    Async tests show that get_sets and get_spoilers act as expected.

--- modules/mtgSpoilers.py
+++ modules/mtgSpoilers.py
@@ -3,12 +3,11 @@ import discord
 from discord.ext import commands  # , tasks
 import aiohttp
 import asyncio
+from datetime import date
 
 
 TOKEN = 'YOUR_BOT_TOKEN'
 CHANNEL_ID = 123456789012345678  # Replace with your desired channel ID
-
-bot = commands.Bot(command_prefix='!')
 
 
 async def get_latest_sets(session):
@@ -16,21 +15,21 @@ async def get_latest_sets(session):
         if response.status == 200:
             data = await response.json()
             return {set_data['code']: set_data['name'] for set_data in data['data']
-                    if set_data['set_type'] in ['expansion', 'core']}
+                    if date.fromisoformat(set_data['released_at']) > date.today()}
         return {}
 
 
 async def get_new_spoilers(session, set_code):
-    async with session.get(
-            f'https://api.scryfall.com/cards/search?order=spoiled&q=e%3A{set_code}+is%3Aspoiler'
-            ) as response:
+    flags = '&order=spoiled&unique=prints'
+    url = 'https://api.scryfall.com/cards/search?q=e%3A' + set_code + flags
+    async with session.get(url) as response:
         if response.status == 200:
             data = await response.json()
             return data['data']
         return []
 
 
-async def post_spoilers(spoilers):
+async def post_spoilers(spoilers, bot):
     channel = bot.get_channel(CHANNEL_ID)
     for spoiler in spoilers:
         embed = discord.Embed(
@@ -42,31 +41,36 @@ async def post_spoilers(spoilers):
         await channel.send(embed=embed)
 
 
-@bot.event
-async def on_ready():
-    print(f'We have logged in as {bot.user}')
+def main():
+    intents = discord.Intents.default()
+    intents.messages = True
+    intents.guilds = True
+    bot = commands.Bot(command_prefix='!', intents=intents)
+    bot.run(TOKEN)
 
-    tracked_sets = {}
-    already_spoiled = set()
+    @bot.event
+    async def on_ready():
+        print(f'We have logged in as {bot.user}')
 
-    async with aiohttp.ClientSession() as session:
-        while True:
-            # Update the tracked sets
-            latest_sets = await get_latest_sets(session)
-            for set_code, set_name in latest_sets.items():
-                if set_code not in tracked_sets:
-                    tracked_sets[set_code] = set_name
-                    print(f'Now tracking set {set_name} ({set_code})')
+        tracked_sets = {}
+        already_spoiled = set()
 
-            # Check for new spoilers in each set
-            for set_code in tracked_sets.keys():
-                new_spoilers = await get_new_spoilers(session, set_code)
-                new_spoilers = [spoiler for spoiler in new_spoilers if spoiler['id'] not in already_spoiled]
+        async with aiohttp.ClientSession() as session:
+            while True:
+                # Update the tracked sets
+                latest_sets = await get_latest_sets(session)
+                for set_code, set_name in latest_sets.items():
+                    if set_code not in tracked_sets:
+                        tracked_sets[set_code] = set_name
+                        print(f'Now tracking set {set_name} ({set_code})')
 
-                if new_spoilers:
-                    await post_spoilers(new_spoilers)
-                    already_spoiled.update(spoiler['id'] for spoiler in new_spoilers)
+                # Check for new spoilers in each set
+                for set_code in tracked_sets.keys():
+                    new_spoilers = await get_new_spoilers(session, set_code)
+                    new_spoilers = [spoiler for spoiler in new_spoilers if spoiler['id'] not in already_spoiled]
 
-            await asyncio.sleep(3600)  # Check for new sets and spoilers every hour
+                    if new_spoilers:
+                        await post_spoilers(new_spoilers, bot)
+                        already_spoiled.update(spoiler['id'] for spoiler in new_spoilers)
 
-bot.run(TOKEN)
+                await asyncio.sleep(3600)  # Check for new sets and spoilers every hour
--- tests/test_temp.py
+++ tests/test_temp.py
@@ -2,7 +2,7 @@
 Template Test Cases, please update
 """
 import aiohttp
-from unittest import TestCase
+from unittest import IsolatedAsyncioTestCase as TestCase
 from modules import mtgSpoilers
 
 class TestSpoilers(TestCase):
@@ -12,6 +12,11 @@ class TestSpoilers(TestCase):
     def setUpClass(cls):
         """ Setup some variables """
         cls.expected_sets = {'ltc','ltr','cmm','mat','mul','moc','mom','sis','sir'}
+        cls.expected_cards = {
+            "8dc1b381-fbc4-451c-a468-2beaa344a581",
+            "601402f4-8647-4c41-9a0f-63d10ce89d02",
+            "5805f64c-dd88-4e94-8f0a-a01dae67e3ba"
+        }
 
     @classmethod
     def tearDownClass(cls):
@@ -27,11 +32,19 @@ class TestSpoilers(TestCase):
     #   T E S T  C A S E S
     ###########################################################################
 
-    def test_get_sets(self):
+    async def test_get_sets(self):
         """ It should return a dictionary of sets that aren't released yet """
         session = aiohttp.ClientSession()
         set_codes = set()
-        result = mtgSpoilers.get_latest_sets(session)
-        set_codes |= {result.keys()}
+        result = await mtgSpoilers.get_latest_sets(session)
+        set_codes |= {key for key in result.keys()}
         self.assertEqual(self.expected_sets, set_codes)
-        
+
+
+    async def test_get_spoilers(self):
+        """ It should return a list of cards in a set """
+        session = aiohttp.ClientSession()
+        card_codes = set()
+        result = await mtgSpoilers.get_new_spoilers(session, 'ltc')
+        card_codes |= {card_data['id'] for card_data in result}
+        self.assertLessEqual(self.expected_cards, card_codes)
